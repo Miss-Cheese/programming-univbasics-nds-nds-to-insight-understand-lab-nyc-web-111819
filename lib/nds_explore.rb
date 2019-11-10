b8d59d8e89d045fcd57fd90d58ab586a62275082
Natalie Galligan <nat.galligan@gmail.com>
2019-11-10 01:20:01 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -10,15 +10,6 @@ end
 
 
 def print_first_directors_movie_titles
-  # row_index = 0
-  # element_index = 0
-  # while element_index < directors_database[row_index].count do
-  #   puts directors_database[:name][:movies][:title]
-  #   element_index += 1
-  # end
-  # puts directors_database[0][:movies][0][:title]
-  # puts directors_database[0][:movies][1][:title]
-
   spielberg_movies = directors_database[0][:movies]
   counter = 0
 
